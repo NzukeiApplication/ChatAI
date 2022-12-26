0c41d2c03a922c36481a023df131443f26ca1dfc
Nzukei <sawoosoo1004@gmail.com>
2022-12-27 04:56:37 +0900

    Initial commit

---
+++ ChatAI/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ChatAI
+//
+//  Created by 신도형 on 2022/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
